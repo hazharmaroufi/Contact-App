4ad68ec9eb8efed7e74c4453b4a978ccc8a26633
Hazhar Maroufi <hazhmar@gmail.com>
2024-11-21 10:35:29 +0330

    header added

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <>
+      <header>
+        <h1 className="h1">Contact App</h1>
+        <p className="para">Hazhar Maroufi</p>
+      </header>
+    </>
+  );
+}
+
+export default Header;
